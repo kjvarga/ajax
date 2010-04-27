5ceb5c0ab50c664893fcad218b916a8b5946a199
Karl Varga <kjvarga@gmail.com>
2010-04-27 13:26:39 -0700

    Never return to the framework path

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -1,3 +1,14 @@
 class AjaxController < ApplicationController
-  unloadable
+  after_filter :clear_return_to
+
+  # Don't return to the framework path, return to root
+  def clear_return_to
+    return unless Ajax.is_enabled?
+    if session[:return_to] =~ %r[#{Ajax.framework_path}]
+      session[:return_to] = session[:return_to].sub(%r[#{Ajax.framework_path}], '/')
+      Rails.logger.info("[ajax] return_to / instead of #{Ajax.framework_path}")
+    end
+  end
+
+  unloadable # needed when installed as a plugin
 end
