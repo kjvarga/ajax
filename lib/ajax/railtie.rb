25131e996af22208bb14e656fbe473a0a4e36a43
jkongie <jkongie@gmail.com>
2011-03-22 17:12:39 +1100

    Fix module name in railtie

--- lib/ajax/railtie.rb
+++ lib/ajax/railtie.rb
@@ -1,4 +1,4 @@
-module SitemapGenerator
+module Ajax
   class Railtie < Rails::Railtie
     rake_tasks do
       load(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'tasks', 'ajax_tasks.rake')))
