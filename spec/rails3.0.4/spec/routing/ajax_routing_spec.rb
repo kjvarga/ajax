d7d8c2b075c6f0dd35ae2fd8b481043ec094e0fe
Karl Varga <kjvarga@gmail.com>
2011-05-09 17:15:49 -0700

    Removing the test Rails 3.0.4 app.  Moving to its own repo

--- spec/rails3.0.4/spec/routing/ajax_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe AjaxController do
-  describe "routing" do
-    it "recognizes and generates #framework" do
-      { :get => "/ajax/framework" }.should route_to(:controller => "ajax", :action => "framework")
-    end
-  end
-end
