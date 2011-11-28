6ff7bf79da52d078acd03f945964667e1e247ef2
Karl Varga <kjvarga@gmail.com>
2011-11-28 12:00:09 -0800

    Simplify the Gemfile dependencies

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 source "http://rubygems.org"
 
-gem 'rails', '=3.1'
-gem 'bundler', '~> 1.0'
+gem 'rails', '>=3.1'
 gem 'ajax', :path => "./"
+gem 'bundler'
 
-gem 'ruby-debug', '=0.10.3'
-gem 'ruby-debug-base', '=0.10.3'
-gem 'rspec', '~>2.0'
-gem 'mocha', '~>0.9.10'
-gem 'test-unit', '=1.2.3'
+group :development, :test do
+  gem 'mocha'
+  gem 'rspec'
+  gem 'ruby-debug'
+  gem 'ruby-debug-base'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,5 +10,9 @@ Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].e
 RSpec.configure do |config|
   config.include(Ajax::RSpec::RequestHelpers)
   config.mock_with :mocha
-  # config.filter_run :focus => true
+
+  # Pass :focus option to +describe+ or +it+ to run that spec only
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
