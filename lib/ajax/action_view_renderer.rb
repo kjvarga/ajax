e83210d107e7176841cb1c97ec03279cde01651d
Karl Varga <kjvarga@gmail.com>
2011-12-08 16:32:38 -0800

    Only set the layout variable once, otherwise we end up overriding it

--- lib/ajax/action_view_renderer.rb
+++ lib/ajax/action_view_renderer.rb
@@ -1,7 +1,11 @@
+# Track rendering of layouts for Rails >= 3.1.  This is called each time something
+# is rendered.  The main layout is rendered first.
 ActionView::Renderer.class_eval do
   if !instance_methods.include?('render_with_tracking')
     def render_with_tracking(context, options)
-      context.controller.instance_variable_set(:@_rendered_layout, options[:layout])
+      if options[:layout] && !context.controller.instance_variable_get(:@_rendered_layout)
+        context.controller.instance_variable_set(:@_rendered_layout, options[:layout])
+      end
       render_without_tracking(context, options)
     end
     alias_method_chain :render, :tracking
